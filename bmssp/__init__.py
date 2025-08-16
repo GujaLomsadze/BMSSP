9a782d7674b1a708614e58a82c49a2a5f4f848f8
GujaLomsadze <lomsadze.guja@gmail.com>
2025-08-17 01:21:55 +0200

    ADD: Pyproj.toml and Setup configuration

---
+++ bmssp/__init__.py
@@ -0,0 +1,16 @@
+"""
+BMSSP: Bounded Multi-Source Shortest Path Algorithm
+
+A Python implementation of the breakthrough O(m log^(2/3) n) shortest path algorithm
+that breaks the sorting barrier for directed graphs.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Guja Lomsadze"
+__email__ = "lomsadze.guja@gmail.com"
+
+""" This will be hte API for now basically """
+# from .algorithms.bmssp import BMSSP
+# from .algorithms.shortest_path import single_source_shortest_path
+
+# __all__ = ["BMSSP", "single_source_shortest_path"]
